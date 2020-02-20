a634921375cfae221edacd570df9a18626bf9929
Pekomon <5183385+pekomon@users.noreply.github.com>
2020-02-20 09:16:03 +0200

    MartialArtsWeapon power can't be negative

--- src/com/example/pekomon/streetfighter/weapon/MartialArtWeapon.java
+++ src/com/example/pekomon/streetfighter/weapon/MartialArtWeapon.java
@@ -4,6 +4,8 @@ import com.example.pekomon.streetfighter.util.RandomizationUtils;
 
 public class MartialArtWeapon implements Weapon {
 
+    private final static int POWER_THRESHOLD = 2;
+
     private final String name;
     private final int power;
     public MartialArtWeapon(final String pName, final int pPower) {
@@ -11,9 +13,14 @@ public class MartialArtWeapon implements Weapon {
         power = pPower;
     }
 
+    /**
+     * Returns the power of weapon. Includes some variance but is always greater than zero.
+     * @return, int, describing the power of weapon in this specific attack. Value may be different everytime but
+     * it is never negative.
+     */
     @Override
     public int getPower() {
-        return power + RandomizationUtils.getNumber(-2, 2);
+        return Math.max(0, power + RandomizationUtils.getNumber(-POWER_THRESHOLD, POWER_THRESHOLD));
     }
 
     @Override
